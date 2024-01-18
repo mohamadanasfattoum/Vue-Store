5551af9c0f542b53dded1ab6eb4f03686d5e3078
Mohamad anas fattoum <samaanas30@gmail.com>
2024-01-18 23:35:03 +0100

    update components

---
+++ src/components/CourseFilter.vue
@@ -0,0 +1,12 @@
+
+<template>
+<h3>CourseFilter</h3>
+</template>
+
+<script>
+
+  export default {
+    name: 'CourseFilter'
+  }
+
+</script>
---
+++ src/components/CourseList.vue
@@ -0,0 +1,11 @@
+<template>
+<h3>Course List</h3>
+</template>
+
+
+
+<script>
+  export default {
+    name: 'CourseList'
+  }
+</script>
