e970d35bca1d4082352934ea8415105bf333fc5c
Mohamad anas fattoum <samaanas30@gmail.com>
2024-01-20 17:00:40 +0100

    show and import CourseList and CourseFilter in courses

--- src/views/CoursesView.vue
+++ src/views/CoursesView.vue
@@ -1,11 +1,28 @@
 
 <template>
   <h3>Courses Page</h3>
+
+  <div>
+    <CourseFilter />
+  </div>
+
+  <div>
+    <CourseList />
+  </div>
+
 </template>
 
 
 <script>
+
+  import CourseList from '@/components/CourseList.vue';
+  import CourseFilter from '@/components/CourseFilter.vue';
+
   export default {
-    name: 'CoursesPage'
+    name: 'CoursesPage',
+    components: {
+      CourseList,
+      CourseFilter
+    }
   }
 </script>
