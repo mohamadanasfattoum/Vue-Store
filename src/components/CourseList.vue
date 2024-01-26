2c0aea9d27ff97c58790f42c1ca7368819ca5d92
Mohamad anas fattoum <samaanas30@gmail.com>
2024-01-26 01:54:31 +0100

    add card in corses list

--- src/components/CourseList.vue
+++ src/components/CourseList.vue
@@ -1,5 +1,17 @@
 <template>
-<h3>Course List</h3>
+<div class="row mt-5">
+  <div class="col-lg-3">
+    <div class="card">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <RouterLink class="btn btn-primary" to="/course-detail">Course Detail</RouterLink>
+
+      </div>
+    </div>
+  </div>
+
+</div>
 </template>
 
 
