4909bf2f90a1a2f3561f4f70b6fb00b7e68342b3
Mohamad anas fattoum <samaanas30@gmail.com>
2024-02-07 12:02:10 +0100

    install primevue and import in main.js

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import PrimeVue from 'primevue/config';
+
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap'
 import 'bootstrap-icons/font/bootstrap-icons.css'
@@ -13,3 +15,5 @@ const app = createApp(App)
 app.use(router)
 
 app.mount('#app')
+
+app.use(PrimeVue);
