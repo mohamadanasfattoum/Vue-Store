5550d33a7ca615cc28d01452009900ffe8d06e33
Mohamad anas fattoum <samaanas30@gmail.com>
2024-02-07 14:45:16 +0100

    update bootstrap

--- src/main.js
+++ src/main.js
@@ -6,9 +6,9 @@ import router from './router'
 
 import PrimeVue from 'primevue/config';
 
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap'
-import 'bootstrap-icons/font/bootstrap-icons.css'
+import 'bootstrap/dist/css/bootstrap-grid.min.css'
+// import 'bootstrap'
+// import 'bootstrap-icons/font/bootstrap-icons.css'
 
 import 'primevue/resources/themes/aura-light-green/theme.css'
 import Button from "primevue/button"
